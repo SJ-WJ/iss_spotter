05a014ff56b14a46720645da69c93e64301018d5
Siraj <sirajwahaj.ibrahim@gmail.com>
2022-07-08 02:51:47 +0000

    Fetch latitude and longitude

--- iss.js
+++ iss.js
@@ -7,12 +7,13 @@
  *   - The IP address as a string (null if error). Example: "162.245.144.188"
  */
 const request = require('request');
-const url = 'https://api.ipify.org?format=json';
+const ipAddress = 'https://api.ipify.org?format=json';
+const geoLocation = "http://ipwho.is/";
 
 
 const fetchMyIP = function(callback) {
   // use request to fetch IP address from JSON API
-  request(url, (error, response, body) => {
+  request(ipAddress, (error, response, body) => {
     if (error) {
       callback(error, null);
       return;
@@ -26,6 +27,32 @@ const fetchMyIP = function(callback) {
     const ip = JSON.parse(body);
     callback(null, ip);
   });
+
 };
 
-module.exports = { fetchMyIP };
+const fetchCoordsByIP = function(ip, callback) {
+
+  request(geoLocation, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+
+    const parsedBody = JSON.parse(body);
+    if (!parsedBody.success) {
+      const message = `Success status was ${parsedBody.success}. Server message says: ${parsedBody.message} when fetching for IP ${parsedBody.ip}`;
+      callback(Error(message), null);
+      return;
+    } 
+
+    //This part ...
+    const {latitude, longitude} = parsedBody;
+    // callback(null, {latitude, longitude});
+    console.log(null, {latitude, longitude});
+
+  })
+    
+};
+
+
+module.exports = { fetchMyIP, fetchCoordsByIP };
